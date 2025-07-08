0c2194f0edab932ef1894cae1bdb25e7dfac34aa
gustavopvilela <gustavohp.vilela@gmail.com>
2025-07-07 21:23:23 -0300

    [Fix]

--- src/main.jsx
+++ src/main.jsx
@@ -2,22 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from "redux-persist/integration/react";
-import { BrowserRouter } from 'react-router-dom';
+import {BrowserRouter, HashRouter} from 'react-router-dom';
 import App from './App';
 import store, {persistor} from './store';
 import './styles/globals.css';
 import LoadingSpinner from "./components/LoadingSpinner.jsx";
 
-// const basename = import.meta.env.PROD ? '/kanban' : '/kanban';
+//const basename = import.meta.env.PROD ? '/kanban' : '/kanban';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <Provider store={store}>
             <PersistGate loading={<LoadingSpinner message="Carregando dados..." />} persistor={persistor}>
-                <BrowserRouter>
+                <HashRouter>
                     <App />
-                </BrowserRouter>
+                </HashRouter>
             </PersistGate>
         </Provider>
     </React.StrictMode>
